f7d4561273b8c6fb2af46b7382e3ff37d3793d03
Horváth Dániel <horvathdaniel783@gmail.com>
2026-04-18 21:22:27 +0200

    MagicNumbers  megoldas 1

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -1,10 +1,48 @@
 from pathlib import Path
 
 
+def next_magic_num(num):
+    strNum = str(num)
+    numLen = len(strNum)
+
+    if strNum == "9" * numLen:
+        return num + 2
+    
+    if numLen % 2 == 0:
+        middle = numLen // 2
+    else:
+        middle = (numLen // 2) + 1
+
+    nums = list(strNum)
+
+    for i in range(middle):
+        nums[numLen-1-i] = nums[i]
+    
+    mirroredNum = "".join(nums)
+
+    if mirroredNum <= strNum:
+        left = str(int(strNum[:middle])+1)
+        if numLen % 2 == 0:
+            return int(left + left[::-1])
+        else:
+            return int(left + left[:-1][::-1])
+        
+    return int(mirroredNum)
+
+
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
-    print(data, end="")
 
+    for og_line in data.splitlines():
+        line = og_line.strip()
+        if not line:
+            continue
+        if '^' in line:
+            left_side, right_side = line.split('^')
+            num = pow(int(left_side), int(right_side))
+        else:
+            num = int(line)
+        print(og_line + " => " + str(next_magic_num(num)))
 
 if __name__ == "__main__":
     main()
