d9099304d361378e8bb4cf6aa9691f61d2c32d97
Horváth Dániel <horvathdaniel783@gmail.com>
2026-04-18 21:39:14 +0200

    MagicNumbers megoldas 3

--- magic_numbers/main.py
+++ magic_numbers/main.py
@@ -8,27 +8,20 @@ def next_magic_num(num):
     if strNum == "9" * numLen:
         return num + 2
     
+    half = (numLen + 1) // 2
+    left = strNum[:half]
+
     if numLen % 2 == 0:
-        middle = numLen // 2
+        mirroredNum = left + left[::-1]
     else:
-        middle = (numLen // 2) + 1
-
-    nums = list(strNum)
-
-    for i in range(middle):
-        nums[numLen-1-i] = nums[i]
-    
-    mirroredNum = "".join(nums)
-
+        mirroredNum = left + left[-2::-1]
     if mirroredNum <= strNum:
-        left = str(int(strNum[:middle])+1)
+        left = str(int(left) + 1)
         if numLen % 2 == 0:
             return int(left + left[::-1])
         else:
-            return int(left + left[:-1][::-1])
-        
+            return int(left + left[-2::-1])
     return int(mirroredNum)
-
 
 def main():
     data = Path("input.txt").read_text(encoding="utf-8")
@@ -39,8 +32,8 @@ def main():
         if not line:
             continue
         if '^' in line:
-            left_side, right_side = line.split('^')
-            num = pow(int(left_side), int(right_side))
+            base, exp = line.split('^', 1)
+            num = pow(int(base), int(exp))
         else:
             num = int(line)
         print(next_magic_num(num))
